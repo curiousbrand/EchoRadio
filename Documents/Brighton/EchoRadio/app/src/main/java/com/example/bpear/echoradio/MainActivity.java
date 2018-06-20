b5d1d0cfbfb0f05a26569c3fa959df92aea665a4
Brandon Peart <bpeart7@gmail.com>
2018-06-20 20:14:15 +0100

    Still need to get play/pause button to change

--- Documents/Brighton/EchoRadio/app/src/main/java/com/example/bpear/echoradio/MainActivity.java
+++ Documents/Brighton/EchoRadio/app/src/main/java/com/example/bpear/echoradio/MainActivity.java
@@ -11,101 +11,120 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-
-        private Toolbar radiotoolbar;
-        ImageView b_play1;
-        MediaPlayer mediaPlayer;
-        boolean prepared;
-        String stream = "https://tunein.com/radio/Radio-X-UK-s254741/";
-        private boolean started;
-
-        @Override
-        protected void onCreate (Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            radiotoolbar = findViewById(R.id.Radio_toolbar);
-            radiotoolbar.setTitle("Live Stream");
-            b_play1 = findViewById(R.id.buttonPlay);
-            b_play1.setEnabled(false);
-
-            mediaPlayer = new MediaPlayer();
-            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-            new PlayerTask().execute(stream);
-
-            b_play1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (started) {
-                        started = false;
-                        mediaPlayer.pause();
-                    } else {
-                        started = true;
-                        mediaPlayer.start();
+    private boolean started;
+    int length = 0;
+    boolean pause;
+    boolean prepared;
+    private Toolbar radiotoolbar;
+    Button b_play1;
+    MediaPlayer mediaPlayer;
+    String stream = "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p";
 
 
-                    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        radiotoolbar = findViewById(R.id.Radio_toolbar);
+        radiotoolbar.setTitle("Live Stream");
+
+        b_play1 = findViewById(R.id.play_or_pause);
+        b_play1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.print("clicked");
+                if (started) {
+                    // Paused
+                    started = false;
+                  //  b_play1.setImageResource(R.drawable.ic_pause_black_24dp);// the pause button will
+                    length = mediaPlayer.getCurrentPosition();
+                    mediaPlayer.pause();
+                } else {
+                    //resume
+                    started = true;
+                  //  b_play1.setImageResource(R.drawable.ic_play_arrow_black_24dp);    // The play button will be the image shown when it is resumed
+//                    mediaPlayer.seekTo(length);
+//                    mediaPlayer.start();
+                    System.out.print("VideoStartPreparing");
+                    new PlayerTask().execute(stream);
                 }
-            });
-
-            BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-            navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        }
-
-        @Override
-        protected void onPause () {
-            super.onPause();
-            if (started) {
-                mediaPlayer.pause();
             }
-        }
+        });
 
-        @Override
-        protected void onResume () {
-            super.onResume();
-            if (started) {
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener()
+
+        {
+            public void onPrepared (MediaPlayer mediaPlayer){
+                System.out.print("VideoPrepared");
                 mediaPlayer.start();
             }
+        });
+
+
+
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (started) {
+            mediaPlayer.pause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (started) {
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (prepared) {
+            mediaPlayer.release();
+        }
+    }
+
+    class PlayerTask extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            try {
+                mediaPlayer.setDataSource(stream);
+                mediaPlayer.prepareAsync();
+                prepared = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return prepared;
         }
 
         @Override
-        protected void onDestroy () {
-            super.onDestroy();
-            if (prepared) {
-                mediaPlayer.release();
-            }
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            b_play1.setEnabled(true);
         }
-
-
-
-
-        class PlayerTask extends AsyncTask<String, Void, Boolean> {
-            @Override
-            protected Boolean doInBackground(String... strings) {
-                try {
-                    mediaPlayer.setDataSource(strings[0]);
-                    mediaPlayer.prepare();
-                    prepared = true;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                return prepared;
-            }
-
-            @Override
-            protected void onPostExecute(Boolean aBoolean) {
-                super.onPostExecute(aBoolean);
-                b_play1.setEnabled(true);
-            }
-        }
-
+    }
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
